a200b3ed28c26e9f362061a6511bbe1354c1f91c
mihail <sapegin2005@yandex.ru>
2025-03-13 12:26:13 +0500

    Добавление кастомной авторизации

--- BackendPortfolio/src/main/java/ru/project/BackendPortfolio/config/SecurityConfig.java
+++ BackendPortfolio/src/main/java/ru/project/BackendPortfolio/config/SecurityConfig.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.project.BackendPortfolio.services.PersonDetailsService;
 
@@ -19,6 +22,24 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Autowired
     public SecurityConfig(PersonDetailsService personDetailsService) {
         this.personDetailsService = personDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //TODO настроить защиту от подделки запросов (CSRF)
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                                .requestMatchers("/auth/login", "/error").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/hello", true)
+                        .failureUrl("/auth/login?error")
+                );
+        return http.build();
     }
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
